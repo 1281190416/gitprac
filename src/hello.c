efa98b780e6b95eccc207637363a0ca44fdc7364
dlw <1281190416@qq.com>
2018-11-28 10:40:21 +0800

    add a new file hello.c

---
+++ src/hello.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: geeker
+    > Mail: 932834897@qq.com 
+    > Created Time: Wed 28 Nov 2018 10:39:06 AM PHT
+ ************************************************************************/
+
+#include<stdio.h>
+int main({
+		printf("git practice\n");
+		return 1;
+		})
